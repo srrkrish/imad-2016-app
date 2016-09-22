508aae108bdeca338cc903d741857267ef75c962
srrkrish <srrkrish@gmail.com>
2016-09-22 22:50:55 +0530

    made some changes locally

--- server.js
+++ server.js
@@ -5,6 +5,45 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate (data) {
+   var title = data.title;
+   var date = data.date;
+   var heading = data.heading;
+   var content = data.content;
+
+   var htmlTemplate = `
+   <html>
+     <head>
+        <title>
+           ${title}
+        </title>
+        <meta name=".viewport" content="width-device-width, initial-scale=1"/>
+        <link href="ui/style.css" rel="stylesheet" />
+     </head>
+     <body>
+        <div class="container">
+           <div>
+	      <a href="/">Home</a>
+	   </div>
+	   <hr/>
+	   <h3>
+	   <div>
+   	      ${heading}
+	   </div>
+	   </h3>
+	   <div>
+	      ${date}
+	   </div>
+	   <div>
+	      ${content}
+	   </div>
+        </div>
+      </body>
+   </html>
+   `;
+   return htmlTemplate;
+}
+
 var articles ={
    'article-one': {
 		title: 'Article One | Radhakrishnan',
@@ -50,45 +89,15 @@ var articles ={
 	`
 	}
 };
-function createTemplate (data) {
-   var title = data.title;
-   var date = data.date;
-   var heading = data.heading;
-   var content = data.content;
-
-   var htmlTemplate = `
-   <html>
-     <head>
-        <title>
-           ${title}
-        </title>
-        <meta name=".viewport" content="width-device-width, initial-scale=1"/>
-        <link href="ui/style.css" rel="stylesheet" />
-     </head>
-     <body>
-        <div class="container">
-           <div>
-	      <a href="/">Home</a>
-	   </div>
-	   <hr/>
-	   <h3>
-	      ${heading}
-	   </h3>
-	   <div>
-	      ${date}
-	   </div>
-	   <div>
-	      ${content}
-	   </div>
-        </div>
-      </body>
-   </html>
-   `;
-   return htmlTemplate;
-}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+	counter = counter + 1;
+  res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -105,6 +114,10 @@ app.get('/ui/myphoto.jpg', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/bubbles.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bubbles.js'));
 });
 
 app.get('/:articleName', function (req, res) {
--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,6 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 
-//element.innerHTML="New value";
-
 var img=document.getElementById('me');
 var marginLeft = 0;
 function moveRight () {
@@ -12,4 +10,23 @@ function moveRight () {
 }
 img.onclick=function () {
    var interval = setInterval(moveRight,50);
+};
+
+var button = document.getElementById('counter');
+
+button.onclick = function () {
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function () {
+	if (request.readyState === XMLHttpRequest.DONE) {
+		if (request.status === 200) {
+			var counter = request.response.Text;
+			var span = document.getElementById('count');
+			span.innerHTML = counter.toString();
+		}
+	}
+	};
+//	request.open('GET','http://srrkrish.imad.hasura-app.io/counter',true);
+	request.open('GET','http://localhost:8080/counter',true);
+	request.send(null);
+	
 };
