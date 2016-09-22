f8b49df52ba2001f87b75435b58c7cca603998f2
srrkrish <srrkrish@gmail.com>
2016-09-22 22:52:40 +0530

    made some changes locally

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick = function () {
 		}
 	}
 	};
-//	request.open('GET','http://srrkrish.imad.hasura-app.io/counter',true);
-	request.open('GET','http://localhost:8080/counter',true);
+	request.open('GET','http://srrkrish.imad.hasura-app.io/counter',true);
 	request.send(null);
 	
 };
