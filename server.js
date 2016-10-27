3db6503bf9b93e39c3b9972c1989182eb3f1d77b
srrkrish <srrkrish@gmail.com>
2016-10-27 20:14:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/test-db', function (req, res) {
        if (err) {
            rec.status(500).send(err.toString());
        } else {
-	       res.send(JSON.stringify(result));
+	       res.send(JSON.stringify(result.rows));
        }
     });
 });
