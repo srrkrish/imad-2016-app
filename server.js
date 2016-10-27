f94c0f29069d42a55867ee9fc7b0fbc4be2ce053
srrkrish <srrkrish@gmail.com>
2016-10-27 23:24:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/submit-name', function (req, res) {
 	res.send(JSON.stringify(names));
 });
 
-app.get('/articles/:articleName', function (req, res) {
+app.get(':articleName', function (req, res) {
         pool.query("SELECT * FROM article where title = $1", [req.params.articleName], function (err, result) {
         if (err){ 
            res.status(500).send(err.toString());
