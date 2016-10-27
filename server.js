ce7f70e909f9be33aa8e0169d85f7fe9e4266321
srrkrish <srrkrish@gmail.com>
2016-10-27 21:44:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,20 +124,7 @@ app.get('/submit-name', function (req, res) {
 	res.send(JSON.stringify(names));
 });
 
-app.get('/articles/:articleName', function (req, res) {
-    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
-        if (err) {
-           res.status(500).send(err.toString());
-        } else {
-         if(result.rows.length === 0){
-             res.status(404).send('Article Not Found');
-        } else {
-             var articleData=result.rows[0];
-             res.send(createTemplate(articleData));
-            }
-         }
-    });
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
