1ae6a9c3cc51fe457538d9d13b6c744f79b42462
srrkrish <srrkrish@gmail.com>
2016-10-28 15:16:15 +0530

    made some changes locally

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var Pool = require('pg').Pool;
 var config = {
     user: 'srrkrish',
     database: 'srrkrish',
-    host: 'db.imad.hasura-app.io',
+    host: 'localhost',
     port: '5432',
     password: process.env.DB_PASSWORD
 }
