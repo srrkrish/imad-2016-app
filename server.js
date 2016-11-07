02d56895aed705a1619e0c1cb53dae750429d4ae
srrkrish <srrkrish@gmail.com>
2016-11-07 22:59:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,132 +1,3 @@
-
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-var enfig = require('enfig');
-var env = require('env2')('config.env');
-var Pool = require('pg').Pool;
-
-var config = {
-    user: 'srrkrish',
-    database: 'srrkrish',
-    host: 'localhost',
-    port: '5432',
-    password: process.env.DB_PASSWORD
-}
-
-var app = express();
-app.use(morgan('combined'));
-
-function createTemplate (data) {
-   var title = data.title;
-   var date = data.date;
-   var heading = data.heading;
-   var content = data.content;
-   var content1 = data.content1;
-   var content2 = data.content2;
-   var content3 = data.content3;
-   
-   var htmlTemplate = `
-   <html>
-     <head>
-        <title>
-           ${title}
-        </title>
-        <meta name=".viewport" content="width-device-width, initial-scale=1"/>
-        <link href="ui/style.css" rel="stylesheet" />
-     </head>
-	 <body bgColor="#FFFACF" leftMargin="0" topMargin="0" rightMargin="0" marginheight="0" marginwidth="0">
-     <div class="container">
-        <div>
-			<a href="/">Home</a>
-	   </div>
-	   <hr/>
-	   <h3>
-        <div class="container5">
-   	      ${heading}
-	   </div>
-	   </h3>
-	   <div class="container2">
-	      ${date.toDateString()}
-	   </div>
-	   <div class="container5">
-	      ${content}
-	   </div>
-    </div>
-    </body>
-   </html>
-   `;
-   return htmlTemplate;
-}
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var pool = new Pool(config);
-app.get('/test-db', function (req, res){
-    pool.query('SELECT * FROM article', function (err, result) {
-       if (err){ 
-           res.status(500).send(err.toString());
-       } else {
-           res.send(JSON.stringify(result.rows));
-       }
-    });
-});
-
-var counter = 0;
-app.get('/counter', function (req, res) {
-	counter = counter + 1;
-  res.send(counter.toString());
-});
-
-var names = [];
-app.get('/submit-name', function (req, res) {
-	var name = req.query.name;
-	names.push(name);
-	res.send(JSON.stringify(names));
-});
-
-app.get('/articles/:articleName', function (req, res) {
-        pool.query("SELECT * FROM article where title = 'article-one'",  function (err, result) {
-        if (err){ 
-           res.status(500).send(err.toString());
-        } else {
-          if (result.rows.length === 0){
-              res.status(404).send('Article Not found');
-          } else {
-              var articleData=result.rows[0];
-              res.send(createTemplate(articleData));
-          }
-          }
-        });
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/myphoto.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'myphoto.jpg'));
-});
-
-app.get('/ui/rose1.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'rose1.jpg'));
-});
-
-app.get('/ui/rose2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'rose2.jpg'));
-});
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
-=======
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -215,7 +86,7 @@ app.get('/hash/:input', function(req, res) {
 
 app.post('/create-user', function (req, res) {
    // username, password
-   // {"username": "srrkrish", "password": "password"}
+   // {"username": "srrkrish", "password": "srrkrish"}
    // JSON
    var username = req.body.username;
    var password = req.body.password;
@@ -365,4 +236,3 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
->>>>>>> 2f3d15ff774c59e0f2dab5819fe1e15d4fd8073b
