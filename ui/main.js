912336c37f1515a1812c11b8ae360fd63147fc71
srrkrish <srrkrish@gmail.com>
2016-11-07 23:01:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-
-var img=document.getElementById('me');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft+'px';
-}
-img.onclick=function () {
-   var interval = setInterval(moveRight,50);
-};
-
-var button = document.getElementById('counter');
-
-button.onclick = function () {
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function () {
-	if (request.readyState === XMLHttpRequest.DONE) {
-		if (request.status === 200) {
-			var counter = request.responseText;
-			var span = document.getElementById('count');
-			span.innerHTML = counter.toString();
-		}
-	}
-	};
-//	request.open('GET','http://srrkrish.imad.hasura-app.io/counter',true);
-	request.open('GET','http://localhost:8080/counter',true);
-	request.send(null);
-	
-};
-
-var submit = document.getElementById('submit_btn');
-submit.onclick = function () {
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function () {
-	if (request.readyState === XMLHttpRequest.DONE) {
-		if (request.status === 200) {
-			var names = request.responseText;
-			names = JSON.parse(names);
-			var list ='';
-			for (var i=0;i<names.length;i++) {
-				list += '<li>' + names[i] + '</li>';
-			}
-			var ul = document.getElementById('namelist');
-			ul.innerHTML=list;
-		}
-	}
-	};
-	var nameInput = document.getElementById('name');
-	var name = nameInput.value;
-//	request.open('GET','http://srrkrish.imad.hasura-app.io/submit-name?name=' + name,true);
-	request.open('GET','http://localhost:8080/submit-name?name=' + name,true);
-	request.send(null);
-};
-
-=======
 
 function loadLoginForm () {
     var loginHtml = `
@@ -196,4 +137,3 @@ loadLogin();
 
 // Now this is something that we could have directly done on the server-side using templating too!
 loadArticles();
->>>>>>> 2f3d15ff774c59e0f2dab5819fe1e15d4fd8073b
